07c6a4e0248f7cd59a644ed00edb60ddee57550f
livinglh <390866592@qq.com>
2020-04-24 11:43:54 +0800

    定义通用的返回对象（正确，错误，异常）

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # miaoshaproject
 一个简单的 java springboot 秒杀项目
+
+
+## 定义通用的返回对象（正确，错误，异常）
+1. 定义了一个CommonReturnType,固定使用staus和data返回供前端解析使用
+2. 定义了一个包装类BusinessException统一管理错误码（EmBusinessError）
+3. 在BaseController里定义了一个通用的ExceptionHandler类解决未被controller层吸收的exception，
+使用errCode和errMsg的方式处理了BusinessException异常以及内部不可预知的异常
--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -1,30 +1,44 @@
 package com.miaoshaproject.controller;
 
 import com.miaoshaproject.controller.viewobject.UserVO;
+import com.miaoshaproject.error.BusinessException;
+import com.miaoshaproject.error.EmBusinessError;
+import com.miaoshaproject.response.CommonReturnType;
 import com.miaoshaproject.service.UserService;
 import com.miaoshaproject.service.model.UserModel;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
-public class UserController {
+public class UserController extends BaseController{
 
     @Autowired
     private UserService userService;
 
     @RequestMapping("/get")
     @ResponseBody
-    public UserVO getUser(@RequestParam(name="id") Integer id){
+    public CommonReturnType getUser(@RequestParam(name="id") Integer id) throws BusinessException {
         // 调用service服务
         UserModel userModel = userService.getUserById(id);
 
+        // 若获取的对应用户信息不存在
+        if(userModel == null){
+            throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
+        }
+
         // 将核心领域模型用户对象转化为可供UI使用的viewobject
-        return converFromModel(userModel);
+        UserVO userVO = converFromModel(userModel);
+
+        // 返回通用对象
+        return CommonReturnType.create(userVO);
     }
 
     private UserVO converFromModel(UserModel userModel){
@@ -35,4 +49,6 @@ public class UserController {
         BeanUtils.copyProperties(userModel,userVO);
         return userVO;
     }
+
+
 }
